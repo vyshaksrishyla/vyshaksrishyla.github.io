afce888882d9838387cd600f18b6ef6195d96c36
Vyshak Srishylappa <vyshaksrishyla@gmail.com>
2017-12-30 14:48:06 -0500

    Adding GitHub Calendar

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "vyshaksrishyla");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
